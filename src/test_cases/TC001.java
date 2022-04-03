b208eef30521ffd04d11fd4bc331e25875f022e1
PaulMelnyk <pmelnyk19@gmail.com>
2022-04-02 21:37:47 -0400

    update tc001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -1,12 +1,14 @@
 package test_cases;
 
 public class TC001 {
+    public static void main(String[] args) {
 
 
-    // testing some thing
+        // testing some thing
 
 
-    // more testing
+        // more testing
 
 
+    }
 }
